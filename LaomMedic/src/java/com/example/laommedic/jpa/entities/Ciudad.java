cd83968ac538f25ceb9ed56d430a42213b8b54c3
neney <ralberto115@gmail.com>
2015-09-10 14:02:24 -0500

    cambio en ciudad

--- LaomMedic/src/java/com/example/laommedic/jpa/entities/Ciudad.java
+++ LaomMedic/src/java/com/example/laommedic/jpa/entities/Ciudad.java
@@ -25,7 +25,7 @@ import javax.validation.constraints.Size;
  * @author Rubén.Gómez
  */
 @Entity
-@Table(name = "CIUDADADES")
+@Table(name = "CIUDADADES1")
 @IdClass(value = CiudadPK.class)
 public class Ciudad implements Serializable {
     
